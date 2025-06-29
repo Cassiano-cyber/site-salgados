38098be2e45856c8bc2c844e983b422aa4686c01
Cassiano-cyber <00001095269859SP@al.educacao.sp.gov.br>
2025-06-29 19:32:03 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors({
+  origin: 'https://cassiano-cyber.github.io'
+}));
 app.use(express.json());
 
 const db = require('./firebase');
